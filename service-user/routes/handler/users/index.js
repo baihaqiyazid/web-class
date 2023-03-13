76a4a6b2ab3fd63f9804ad3fc694dc95914f39d2
baihaqiyazid <baihaqiyazid16@gmail.com>
2023-03-13 16:59:03 +0700

    make API logout

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -3,7 +3,8 @@ const login = require('./login')
 const update = require('./update')
 const getUserById = require('./getUserById')
 const getUsers = require('./getUsers')
+const logout = require('./logout')
 
 module.exports = {
-    register, login, update, getUserById, getUsers
+    register, login, update, getUserById, getUsers, logout
 }
--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -6,6 +6,7 @@ const userHandler = require('./handler/users')
 router.post('/register', userHandler.register)
 router.post('/login', userHandler.login)
 router.post('/:id/update', userHandler.update)
+router.post('/logout', userHandler.logout)
 router.get('/:id', userHandler.getUserById)
 router.get('/', userHandler.getUsers)
 
